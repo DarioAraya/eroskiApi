d62bb42d2671d06a8de293919e81993a21a80db0
Dario Araya <dararayag@gmail.com>
2022-04-06 14:34:32 -0400

    Se agrega boton search para buscar por nombre

--- app.js
+++ app.js
@@ -2,32 +2,37 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const conectando = require('./src/mysql_connector.js');
+const ejsMate = require("ejs-mate");
 
-
-
+app.engine('ejs',ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
 
 conectando.connect((err)=>{
     if(err) throw err;
     console.log("Conectado a la base");
 });
 
-app.get('/', (req,res)=>{
-    res.render('home')
+
+app.get('/',(req,res)=>{
+    let sql =  'SELECT * FROM product';
+    conectando.query(sql,  (err, results) => {
+       if(err) throw err;     
+           res.render('eroski/index',{data: results} );    
+      })   
+});
+
+
+app.get('/search', (req,res)=>{
+    let name = req.query.name;
+        var sql = `SELECT * from product where name LIKE '%${name}%'`;
+        conectando.query(sql,(err,results)=>{
+            if(err)throw err;
+            res.render('eroski/index',{data:results});
+        });
 })
 
-app.get('/eroski', async (req,res)=>{
-    const sql = await 'SELECT * FROM product';
-    conectando.query(sql, (error, results)=>{
-        if(error) throw error;
-        if(results.length>0){
-            res.render('eroski/index',{results})
-        }else{
-            res.send('Not results')
-        }
-    })
-});
 
 
 app.listen(3000, ()=>{
