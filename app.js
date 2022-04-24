f18d14000d7bf26d2c1a785607368f259fcecd1e
Dario Araya <dararayag@gmail.com>
2022-04-24 16:48:04 -0400

    primer comit newFront

--- app.js
+++ app.js
@@ -1,66 +1,61 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const ejsMate = require("ejs-mate");
-const catchAsync = require('./utils/catchAsync');
-const axios = require('axios').default;
-const ExpressError = require('./utils/ExpressError');
+const catchAsync = require("./utils/catchAsync");
+const axios = require("axios").default;
+const ExpressError = require("./utils/ExpressError");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.engine('ejs',ejsMate)
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(__dirname + '/public'));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname + "/public"));
 
 //Mostrar todos los productos de la categoria inicial
-app.get('/',catchAsync(async(req,res,next)=>{
-    const response = await axios.get('https://eroski-api-backend.herokuapp.com/all-products');
-    res.render('eroski/index',{...response.data})    
-}));
+app.get(
+  "/",
+  catchAsync(async (req, res, next) => {
+    res.sendFile(path.join(__dirname, "/views/eroski/index.html"));
+  })
+);
 
 //filtrar por nombre
-app.get('/search', catchAsync(async(req,res,next)=>{
-    let nameQuery = req.query.name;
-    const response = await axios.post('https://eroski-api-backend.herokuapp.com/find-for-name',{
-        name: nameQuery
-      })
-    res.render('eroski/index',{...response.data})  
-}));
+app.get(
+  "/search",
+  catchAsync(async (req, res, next) => {
+    res.sendFile(path.join(__dirname, "/views/eroski/search.html"));
+  })
+);
 
 //filtrar por categoria
-app.get('/filter', catchAsync(async(req,res,next)=>{
-    let idQuery = req.query.id;
-    const response = await axios.post('https://eroski-api-backend.herokuapp.com/filter-by-cat',{
-        id: idQuery
-      })
-    res.render('eroski/index',{...response.data})  
-}));
+app.get(
+  "/filter",
+  catchAsync(async (req, res, next) => {
+    res.sendFile(path.join(__dirname, "/views/eroski/filter.html"));
+  })
+);
 
 //ordenar por
-app.get('/sort', catchAsync(async(req,res,next)=>{
-    let sortByQuery = req.query.sortBy;
-    let idQuery = req.query.id;
+app.get(
+  "/sort",
+  catchAsync(async (req, res, next) => {
+    res.sendFile(path.join(__dirname, "/views/eroski/sort.html"));
+  })
+);
 
-    const response = await axios.post('https://eroski-api-backend.herokuapp.com/sort-by',{
-        id: idQuery,
-        sortBy: sortByQuery
-      })
-    res.render('eroski/index',{...response.data})  
-}));
-
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
- res.status(statusCode).render('error', { err })
-})
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh No, Something Went Wrong!";
+  res.status(statusCode).render("error", { err });
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, ()=>{
-    console.log(`Serving on port ${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
+});
---
+++ public/js/search.js
@@ -0,0 +1,146 @@
+const params = (parameterName) => {
+  let parameters = new URLSearchParams(window.location.search);
+  return parameters.get(parameterName);
+};
+const productFather = document.querySelector("#productFather");
+const nameQuery = params("name");
+
+//PRODUCTO
+const buscar = async () => {
+  try {
+    const res = await axios.post("http://localhost:4000/find-for-name", {
+      name: nameQuery,
+    });
+    for (const arr of res.data.products) {
+      productFather.append(nuevoDiv("col-md-4", `div${arr.id}`));
+      document
+        .querySelector(`#div${arr.id}`)
+        .append(nuevoDiv("card mt-3", `div${arr.id}-2`));
+      document
+        .querySelector(`#div${arr.id}-2`)
+        .append(
+          nuevoDiv(
+            "product-1 aling-items-center p-2 text-center",
+            `div${arr.id}-3`
+          )
+        );
+      if (arr.url_image) {
+        document
+          .querySelector(`#div${arr.id}-3`)
+          .append(
+            nuevaImg(`${arr.url_image}`, "rounded", "160", "160", "img1")
+          );
+      } else {
+        document
+          .querySelector(`#div${arr.id}-3`)
+          .append(
+            nuevaImg(
+              "https://www.yeaah.co/wp-content/uploads/2021/08/producto-sin-imagen.jpg",
+              "rounded",
+              "160",
+              "160",
+              "img2"
+            )
+          );
+      }
+      document.querySelector(`#div${arr.id}-3`).append(nuevoH5(`${arr.name}`));
+      //ESTO SIRVE PARA VER LA CATEGORIA DEL ULTIMO ELEMENTO
+      //var type = `${arr.category}`;
+      //FIN
+      document
+        .querySelector(`#div${arr.id}-3`)
+        .append(nuevoDiv("cost mt-3", `div${arr.id}-4`));
+      //ESTO SIRVE PARA PONERLE EL PUNTO AL PRECIO
+      let price = Math.round(arr.price - (arr.price * arr.discount) / 100);
+      let price2 = new Intl.NumberFormat("es-CL", {
+        style: "currency",
+        currency: "CLP",
+      }).format(price);
+      let price3 = new Intl.NumberFormat("es-CL", {
+        style: "currency",
+        currency: "CLP",
+      }).format(arr.price);
+      if (arr.discount > 0) {
+        document
+          .querySelector(`#div${arr.id}-4`)
+          .append(nuevoSpan(`span${arr.id}-1`, ""));
+        document.querySelector(`#span${arr.id}-1`).append(`${price2} `);
+        document
+          .querySelector(`#div${arr.id}-4`)
+          .append(nuevoSpan(`span${arr.id}-2`, ""));
+        document
+          .querySelector(`#span${arr.id}-2`)
+          .append(nuevoDel(`del${arr.id}-1`));
+        document.querySelector(`#del${arr.id}-1`).append(`${price3}`);
+        document
+          .querySelector(`#span${arr.id}-2`)
+          .append(nuevoSpan(`span${arr.id}-3`, ""));
+        document.querySelector(`#span${arr.id}-3`).append(` -${arr.discount}%`);
+      } else {
+        document
+          .querySelector(`#div${arr.id}-4`)
+          .append(nuevoSpan(`span${arr.id}-4`, ""));
+        document.querySelector(`#span${arr.id}-4`).append(`${price3}`);
+      }
+      //FIN
+      document
+        .querySelector(`#div${arr.id}-2`)
+        .append(
+          nuevoDiv(
+            "p-3 product text-center text-white mt-3 cursor",
+            `div${arr.id}-5`
+          )
+        );
+      var type = arr.category;
+      document
+        .querySelector(`#div${arr.id}-5`)
+        .append(nuevoSpan(`span${arr.id}-5`, "text-uppercase"));
+      document.querySelector(`#span${arr.id}-5`).append("Add to cart");
+      var type = arr.category;
+    }
+  } catch (e) {
+    console.log("ERROR", e);
+  }
+};
+//FIN
+
+//CREACION DE ELEMENTOS HTML
+const nuevoDiv = (clase, id) => {
+  const div = document.createElement("DIV");
+  div.setAttribute("class", clase);
+  div.setAttribute("id", id);
+  return div;
+};
+
+const nuevaImg = (src, clase, height, width, id) => {
+  const img = document.createElement("img");
+  img.setAttribute("src", src);
+  img.setAttribute("class", clase);
+  img.setAttribute("height", height);
+  img.setAttribute("width", width);
+  img.setAttribute("id", id);
+  return img;
+};
+
+const nuevoH5 = (nombre) => {
+  const h5 = document.createElement("H5");
+  h5.innerText = nombre;
+  return h5;
+};
+
+const nuevoSpan = (id, clase) => {
+  const span = document.createElement("SPAN");
+  span.setAttribute("id", id);
+  span.setAttribute("class", clase);
+  return span;
+};
+
+const nuevoDel = (id) => {
+  const del = document.createElement("DEL");
+  del.setAttribute("id", id);
+  return del;
+};
+//fin
+
+//EJECUTAR SCRIPT AL INICIAR LA PAGINA.
+buscar();
