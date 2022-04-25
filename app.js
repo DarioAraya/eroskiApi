39d90d12cf76d8e0dae4b2a9e73b576c47c8c2d2
Dario Araya <dararayag@gmail.com>
2022-04-25 16:20:23 -0400

    subiendo2

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(__dirname + "/public"));
 
-const whiteList = ["https://api-eroski.herokuapp.com"];
+const whiteList = ["https://eroski-api-backend.herokuapp.com"];
 
 app.use(cors({ origin: whiteList }));
 
